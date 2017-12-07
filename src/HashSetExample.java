b150679cc5b6b6c05646dffc8ff03db18eb6f67e
Angel <Angel@Angel-HP>
2017-12-06 18:52:17 -0800

    gr

---
+++ src/HashSetExample.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetExample {
+	public static void main(String[] args)
+	{
+		
+		String x = "My Hash Set Example";
+		
+	Set<Integer>mySet= new HashSet<Integer>();
+	//Adding 
+	
+	mySet.add(1);
+	mySet.add(2);
+	mySet.add(2);
+	 System.out.println(x);
+ for (Integer anInt : mySet)
+ {
+	 System.out.println(anInt);
+	
+ }
+	}
+	
+	
+
+}
